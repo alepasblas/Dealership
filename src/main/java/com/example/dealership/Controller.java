b4086406e5a03dc9b9bd34ea9d4b526c22ec49c4
Alejandro Pastor <alepasblas77777@gmail.com>
2024-05-11 13:14:15 +0200

    Upgrade of the login, its full functional now

--- src/main/java/com/example/dealership/Controller.java
+++ src/main/java/com/example/dealership/Controller.java
@@ -1,35 +1,60 @@
 package com.example.dealership;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
 
 public class Controller {
+    public TextField UserInputLogIn;
+    public TextField PasswordInputLogIn;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
+
+
+    public void onEnterLogIn(ActionEvent actionEvent)
+    {
         try {
-            // Cargar el archivo FXML de la nueva escena
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("paginaPrincipal.fxml"));
-            Parent root = loader.load();
+            String user = UserInputLogIn.getText();
+            String password= PasswordInputLogIn.getText();
+            List<String> users= Files.readAllLines(Path.of("src/main/resources/Users.txt"));
+            List<String> passwords= Files.readAllLines(Path.of("src/main/resources/Passwords.txt"));
 
-            // Obtener el controlador de la nueva escena si es necesario
-            // ControladorPaginaPrincipal controller = loader.getController();
+            for(int i=0; i< users.size(); i++)
+            {
+                if(Objects.equals(users.get(i), user) && Objects.equals(passwords.get(i), password))
+                {
+                    // Cargar el archivo FXML de la nueva escena
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("paginaPrincipal.fxml"));
+                    Parent root = loader.load();
 
-            // Obtener el Stage actual
-            Stage stage = (Stage) welcomeText.getScene().getWindow();
 
-            // Establecer la nueva escena en el Stage
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+                    // Obtener el Stage actual
+                    Stage stage = (Stage) welcomeText.getScene().getWindow();
+
+                    // Establecer la nueva escena en el Stage
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                }
+                else
+                {
+                    System.out.println("User or password incorrect");
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace(); // Manejar cualquier error de carga de FXML
         }
